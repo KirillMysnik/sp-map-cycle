8a418997e5c53a02559a446503061efed67ee0a3
KirillMysnik <kirill@mysnik.com>
2016-01-10 02:32:44 +0300

    Fixed lines misplacement in info.py

--- addons/source-python/plugins/sp_map_cycle/info.py
+++ addons/source-python/plugins/sp_map_cycle/info.py
@@ -5,10 +5,10 @@ from plugins.info import PluginInfo
 info = PluginInfo()
 info.name = "SP Map Cycle"
 info.basename = 'sp_map_cycle'
+info.author = 'Kirill "iPlayer" Mysnik'
+info.version = '1.0'
 info.variable = '{}_version'.format(info.basename)
 info.convar = PublicConVar(
     info.variable, info.version, "{} version".format(info.name))
 
-info.author = 'Kirill "iPlayer" Mysnik'
-info.version = '1.0'
 info.url = "https://github.com/KirillMysnik/sp-map-cycle"
